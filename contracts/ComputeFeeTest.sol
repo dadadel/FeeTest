f807600b1c0d362f66babd6118dc17eefe6a8fbd
Adel Daouzli <dadel@hadoly.fr>
2022-05-25 19:39:37 +0200

    update to set the good precision

--- contracts/ComputeFeeTest.sol
+++ contracts/ComputeFeeTest.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.13;
 contract ComputeFeeTest {
     uint256 private constant MAX_BALANCE = 1000000;
     uint256 private constant MAX_BALANCE_50 = MAX_BALANCE / 2;
-    uint256 private constant BPS_100p = 10000;
-    uint256 private constant BPS_5p = 500;
+    uint256 private constant BPS_100p = 100000000;
+    uint256 private constant BPS_5p = 5000000;
 
     /**
      * Compute the 2nd degree polynomial
@@ -12,8 +12,8 @@ contract ComputeFeeTest {
     function _computePoly2(int256 _x) internal pure returns(int256 y) {
         // the coeffifients values and the following division is to keep the
         // coefficients in integer domain and thus avoid usage of fixed point
-        y = 16666666666666480 * _x**2 - 34999999999999863685120 * _x + 18333333333333350527742771200;
-        y /= 1e25;
+        y = 1666666666666646 * _x**2 - 3499999999999984271360 * _x + 1833333333333334228140556288;
+        y /= 1e20;
     }
 
     /**
@@ -23,13 +23,10 @@ contract ComputeFeeTest {
      */
     function computeFee(uint256 _balance) external pure returns(uint256 fee) {
         require(_balance > 0, "Balance cannot be 0");
-        require(_balance <= MAX_BALANCE, "Balance exceeds maximum");
+        require(_balance < MAX_BALANCE, "Balance exceeds maximum allowed");
 
         if(_balance <= MAX_BALANCE_50) {
             fee = BPS_5p;
-        }
-        else if(_balance == MAX_BALANCE) {
-            fee = 0;
         }
         else {
             fee = uint256(_computePoly2(int256(_balance)));
--- test/computeFee.test.js
+++ test/computeFee.test.js
@@ -7,6 +7,8 @@ describe("ComputeFeeTest", function () {
   const MAX_BALANCE = 1000000;
   const BALANCE_80p = 800000;
   const BALANCE_50p = 500000;
+  const BPS_1p = 1000000;
+  const BPS_5p = 5000000;
 
   beforeEach(async function () {
     FeeComputation = await ethers.getContractFactory("ComputeFeeTest");
@@ -15,44 +17,49 @@ describe("ComputeFeeTest", function () {
   });
 
   describe("Fee computation tests", function () {
-    it("should return a fee of 0% when current balance is 100% of the maximum balance", async function () {
+    it("should return a fee of 1.0e-6% when current balance is 100% of the maximum balance", async function () {
       expect(
-        (await contract.computeFee(MAX_BALANCE)).toNumber()
-      ).to.equal(0);
+        (await contract.computeFee(MAX_BALANCE-1)).toNumber()
+      ).to.equal(1);
     });
     it("should return a fee of 1% when current balance is 80% of the maximum balance", async function () {
       expect(
         (await contract.computeFee(BALANCE_80p)).toNumber()
-      ).to.equal(100);
+      ).to.equal(BPS_1p);
     });
     it("should return a fee of 5% when current balance is 50% of the maximum balance", async function () {
       expect(
         (await contract.computeFee(BALANCE_50p)).toNumber()
-      ).to.equal(500);
+      ).to.equal(BPS_5p);
     });
     it("should return the fixed 5% fee for values under 50% of the balance", async function () {
       expect(
         (await contract.computeFee(BALANCE_50p - 1)).toNumber()
-      ).to.equal(500);
+      ).to.equal(BPS_5p);
       expect(
         (await contract.computeFee(BALANCE_50p - 100)).toNumber()
-      ).to.equal(500);
+      ).to.equal(BPS_5p);
       expect(
         (await contract.computeFee(1)).toNumber()
-      ).to.equal(500);
+      ).to.equal(BPS_5p);
     });
     it("should revert if balance is 0", async function () {
       await expect(
         contract.computeFee(0)
       ).to.be.revertedWith("Balance cannot be 0");
     });
+    it("should revert if balance is 100% of max", async function () {
+      await expect(
+        contract.computeFee(MAX_BALANCE)
+      ).to.be.revertedWith("Balance exceeds maximum allowed");
+    });
     it("should revert if balance is over the maximum balance", async function () {
       await expect(
         contract.computeFee(MAX_BALANCE + 1)
-      ).to.be.revertedWith("Balance exceeds maximum");
+      ).to.be.revertedWith("Balance exceeds maximum allowed");
       await expect(
         contract.computeFee(MAX_BALANCE * 2)
-      ).to.be.revertedWith("Balance exceeds maximum");
+      ).to.be.revertedWith("Balance exceeds maximum allowed");
     });
   });
 });
