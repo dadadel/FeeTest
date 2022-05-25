e2d4ee4ed84409f476756494adfb4f0d18a461ea
Adel Daouzli <dadel@hadoly.fr>
2022-05-25 19:41:12 +0200

    update the python script

--- fee_polynom_computation.py
+++ fee_polynom_computation.py
@@ -8,9 +8,9 @@ MIN_INT128 = int(-1.7e38)
 MAX_INT256 = int(5.7e76)
 MIN_INT256 = int(-5.7e76)
 
-BPS_100p = 10000
-BPS_1p = 100
-BPS_5p = 500
+BPS_100p = 100000000
+BPS_1p = 1000000
+BPS_5p = 5000000
 
 MAX_USDT = 1000000
 MAX_USDT_50 = MAX_USDT // 2
@@ -23,7 +23,7 @@ def compute_coefs(points):
     return coefs
 
 def check_overflow(x):
-#    assert MIN_INT256 <= x <= MAX_INT256
+    assert MIN_INT256 <= x <= MAX_INT256
     assert MIN_INT128 <= x <= MAX_INT128
 
 
@@ -37,11 +37,9 @@ def compute_poly(x, a, b, c):
 
 def compute_fee(balance, coefs, coefmult):
     a, b, c = coefs
-    assert balance <= MAX_USDT
+    assert balance < MAX_USDT
     if balance < MAX_USDT_50:
         fee = BPS_5p
-    elif balance >= MAX_USDT:
-        fee = 0
     else:
         fee = compute_poly(balance, a, b, c) // coefmult if coefmult else compute_poly(balance, a, b, c)
     return fee
@@ -57,28 +55,36 @@ def plot_2nd(x, y, coefs, coefmult):
 if __name__ == "__main__":
     points = (MAX_USDT, 0), (MAX_USDT_80, BPS_1p), (MAX_USDT_50, BPS_5p)
     coefs = compute_coefs(points)
-    coefmult = int(1e25)
+    coefmult = int(1e20)
     coefs = int(coefs[0] * coefmult), int(coefs[1] * coefmult), int(coefs[2] * coefmult)
     prev = None
     count = 0
     under = 0
     x = []
     y = []
-    for i in range(MAX_USDT_50+1):
+    total = 0
+    for i in range(1, MAX_USDT_50+1):
         f = compute_fee(MAX_USDT-i, coefs, coefmult)
+        total += 1
         if f != prev:
             x.append(MAX_USDT-i)
-            y.append(f/100)
-            print(MAX_USDT-i,f)
+            y.append(f/BPS_1p)
+            #print(MAX_USDT-i,f)
             count += 1
             prev = f
         if f < 0:
             under += 1
+    print("total:", total)
     print("count of different fee values:", count)
+    print("precision:", count/total)
     print("count of fees under 0:", under)
     print("Coefs:", ", ".join(str(c) for c in coefs))
     print("Coef correction number:", coefmult)
-    print("Fee for 100%:", compute_fee(MAX_USDT, coefs, coefmult))
+    print("Fee for 100%-1:", compute_fee(MAX_USDT-1, coefs, coefmult))
+    print("Fee for 100%-2:", compute_fee(MAX_USDT-2, coefs, coefmult))
+    print("Fee for 80+1%:", compute_fee(MAX_USDT_80+1, coefs, coefmult))
     print("Fee for 80%:", compute_fee(MAX_USDT_80, coefs, coefmult))
+    print("Fee for 80%-1:", compute_fee(MAX_USDT_80-1, coefs, coefmult))
+    print("Fee for 50%+1:", compute_fee(MAX_USDT_50+1, coefs, coefmult))
     print("Fee for 50%:", compute_fee(MAX_USDT_50, coefs, coefmult))
     plot_2nd(x, y, coefs, coefmult)
